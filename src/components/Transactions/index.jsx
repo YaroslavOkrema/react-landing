01cf1a93b3f6d6e237792b127be730836f5d5001
YaroslavOkrema <yaroslav.okrema@gmail.com>
2025-02-21 01:48:44 +0200

    refactor: transactions

--- src/components/Transactions/index.jsx
+++ src/components/Transactions/index.jsx
@@ -29,7 +29,7 @@ const Transactions = () => {
           </div>
         </div>
         <div className="w-[299px] h-[209px] bg-[#6A54FF] px-4 py-1 rounded">
-          <h1 className="font-medium text-xl uppercase mb-1">NEAR/USDT</h1>
+          <h1 className="font-medium text-xl uppercase mb-1">BTC/USDT</h1>
           <p className="text-sm font-medium text-[#a798ff] mb-14">
             SPOT · 1 мин. назад
           </p>
@@ -37,7 +37,27 @@ const Transactions = () => {
           <h2 className="font-medium text-sm uppercase text-[#d2cbfd] mb-1">
             Прибуток
           </h2>
-          <p className="text-3xl font-medium text-[#36fe9f] mb-3">58.6206%</p>
+          <p className="text-3xl font-medium text-[#36fe9f] mb-3">6.02%</p>
+
+          <div className="flex items-center justify-between">
+            <p className="font-medium text-sm uppercase text-[#d2cbfd]">
+              Ціль 3
+            </p>
+            <p className="text-sm font-medium text-[#a798ff]">
+              Дата входа 06.10.2022
+            </p>
+          </div>
+        </div>
+        <div className="w-[299px] h-[209px] bg-[#6A54FF] px-4 py-1 rounded">
+          <h1 className="font-medium text-xl uppercase mb-1">ETH/USDT</h1>
+          <p className="text-sm font-medium text-[#a798ff] mb-14">
+            SPOT · 1 мин. назад
+          </p>
+
+          <h2 className="font-medium text-sm uppercase text-[#d2cbfd] mb-1">
+            Прибуток
+          </h2>
+          <p className="text-3xl font-medium text-[#36fe9f] mb-3">16.3%</p>
 
           <div className="flex items-center justify-between">
             <p className="font-medium text-sm uppercase text-[#d2cbfd]">
@@ -57,27 +77,7 @@ const Transactions = () => {
           <h2 className="font-medium text-sm uppercase text-[#d2cbfd] mb-1">
             Прибуток
           </h2>
-          <p className="text-3xl font-medium text-[#36fe9f] mb-3">58.6206%</p>
-
-          <div className="flex items-center justify-between">
-            <p className="font-medium text-sm uppercase text-[#d2cbfd]">
-              Ціль 4
-            </p>
-            <p className="text-sm font-medium text-[#a798ff]">
-              Дата входа 06.10.2022
-            </p>
-          </div>
-        </div>
-        <div className="w-[299px] h-[209px] bg-[#6A54FF] px-4 py-1 rounded">
-          <h1 className="font-medium text-xl uppercase mb-1">NEAR/USDT</h1>
-          <p className="text-sm font-medium text-[#a798ff] mb-14">
-            SPOT · 1 мин. назад
-          </p>
-
-          <h2 className="font-medium text-sm uppercase text-[#d2cbfd] mb-1">
-            Прибуток
-          </h2>
-          <p className="text-3xl font-medium text-[#36fe9f] mb-3">58.6206%</p>
+          <p className="text-3xl font-medium text-[#36fe9f] mb-3">0.963%</p>
 
           <div className="flex items-center justify-between">
             <p className="font-medium text-sm uppercase text-[#d2cbfd]">
